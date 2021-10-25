42e683929a5bc02a2967a5f70b06c25614304eb8
johncifuentes85 <john.alexcz@gmail.com>
2021-10-25 10:53:07 -0500

    se crea la vista para agregar producto

---
+++ app/src/main/java/com/example/storesale/AddProduct.java
@@ -0,0 +1,16 @@
+package com.example.storesale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProduct extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
